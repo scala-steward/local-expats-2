f86543d843168ff05fa0e7f617bfe3e195e0fc13
Kapil Koju <960364+TheKojuEffect@users.noreply.github.com>
2022-10-09 06:59:10 -0400

    Show post info: state and createdAt date
    Closes #103

--- ui/src/post/Post.tsx
+++ ui/src/post/Post.tsx
@@ -2,24 +2,38 @@ import {FC} from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import {CardActionArea} from '@mui/material';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardHeader from '@mui/material/CardHeader';
 import {PostDto} from "./PostDto";
 
 type PostProps = {
     post: PostDto
 };
 
-export const Post: FC<PostProps> = ({post}) => (
-    <Card sx={{margin: 1}} variant="outlined">
-        <CardActionArea>
-            <CardContent>
-                <Typography gutterBottom variant="h6" component="div">
-                    {post.title}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    {post.message}
-                </Typography>
-            </CardContent>
-        </CardActionArea>
-    </Card>
-);
+export const Post: FC<PostProps> = ({post}) => {
+    const postedDate = new Date(post.createdAt).toLocaleDateString();
+    const postInfo = `posted for ${post.state} on ${postedDate}`;
+    return (
+        <Card sx={{margin: 1}} variant="outlined">
+            <CardActionArea>
+                <CardHeader
+                    title={post.title}
+                    titleTypographyProps={{
+                        variant: "h6",
+                    }}
+                    subheader={postInfo}
+                    subheaderTypographyProps={{
+                        variant: "caption"
+                    }}
+                />
+                <CardContent sx={{mt: -3}}>
+                    <Typography
+                        variant="body1"
+                    >
+                        {post.message}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+    );
+};
