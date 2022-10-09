f32219f5ac9bfb43383a660fa43b0f342d40d0a5
Kapil Koju <960364+TheKojuEffect@users.noreply.github.com>
2022-10-09 05:52:37 -0400

    Remove unused login/logout rewrites

--- ui/next.config.js
+++ ui/next.config.js
@@ -4,18 +4,6 @@ module.exports = {
     rewrites() {
         return [
             {
-                // Separate rewrite config for /api/login so that the cookie is set with path = '/' instead of path = '/api'
-                source: '/login',
-                destination: `${process.env.API_URL}/api/login`,
-                basePath: false
-            },
-            {
-                // Separate rewrite config for /api/logout to remove auth cookie with path = '/'
-                source: '/logout',
-                destination: `${process.env.API_URL}/api/logout`,
-                basePath: false
-            },
-            {
                 source: '/api/:path*',
                 destination: `${process.env.API_URL}/api/:path*`,
                 basePath: false
