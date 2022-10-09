b6bb97062dd5828c6bdb0feeca0877f590f703de
Kapil Koju <960364+TheKojuEffect@users.noreply.github.com>
2022-10-08 22:48:33 -0400

    Remove Search for now

--- ui/src/nav/NavBar.tsx
+++ ui/src/nav/NavBar.tsx
@@ -1,49 +1,11 @@
-import {alpha, styled} from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import AddIcon from '@mui/icons-material/Add';
-import SearchIcon from '@mui/icons-material/Search';
+import Add from '@mui/icons-material/Add';
 import Link from "next/link";
 
-const Search = styled('div')(({theme}) => ({
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': {
-        backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-        marginLeft: theme.spacing(1),
-        width: 'auto',
-    },
-}));
-
-const SearchIconWrapper = styled('div')(({theme}) => ({
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({theme}) => ({
-    color: 'inherit',
-    '& .MuiInputBase-input': {
-        padding: theme.spacing(1, 1, 1, 0),
-        // vertical padding + font size from searchIcon
-        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-        transition: theme.transitions.create('width'),
-        width: '100%',
-    },
-}));
 export default function NavBar() {
     return (
         <Box sx={{flexGrow: 1}}>
@@ -67,15 +29,6 @@ export default function NavBar() {
                         </Link>
                     </IconButton>
                     <Box sx={{flexGrow: 1}}/>
-                    <Search>
-                        <SearchIconWrapper>
-                            <SearchIcon/>
-                        </SearchIconWrapper>
-                        <StyledInputBase
-                            placeholder="Search…"
-                            inputProps={{'aria-label': 'search'}}
-                        />
-                    </Search>
                     <Box sx={{display: {sm: 'flex'}, ml: 2}}>
                         <IconButton
                             size="large"
@@ -83,7 +36,7 @@ export default function NavBar() {
                             color="inherit"
                         >
                             <Link href="/new">
-                                <AddIcon/>
+                                <Add/>
                             </Link>
                         </IconButton>
                     </Box>
