ad7bd4901842c968df4f3be25b8dc693dcb0b3f1
Kapil Koju <960364+TheKojuEffect@users.noreply.github.com>
2022-10-11 10:51:46 -0400

    Ability to go to a specific state from posts

--- ui/src/post/PostSubheader.tsx
+++ ui/src/post/PostSubheader.tsx
@@ -1,18 +1,36 @@
 import {FC} from "react";
 import {PostDto} from "./PostDto";
 import Typography from "@mui/material/Typography";
+import {useSelectedState} from "../location/SelectedState";
+import {stateLabels} from "../nav/State";
+import Chip from "@mui/material/Chip";
 
 type PostSubheaderProps = {
     post: PostDto
 }
 
 export const PostSubheader: FC<PostSubheaderProps> = ({post}) => {
+    const {setSelectedState} = useSelectedState();
     const postedDate = new Date(post.createdAt).toLocaleDateString();
-    const postInfo = `posted in ${post.state} on ${postedDate}`;
-
     return (
-        <Typography variant={"caption"}>
-            {postInfo}
+        <Typography variant="caption">
+            posted in
+            <Chip
+                sx={{mx: 1}}
+                clickable
+                color="primary"
+                label={stateLabels[post.state]}
+                variant="outlined"
+                size="small"
+                onClick={() => setSelectedState(post.state)}
+            />
+            on
+            <Chip
+                sx={{mx: 1}}
+                variant="filled"
+                size="small"
+                label={postedDate}
+            />
         </Typography>
     );
 }
