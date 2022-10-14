efa18204ede340a26a20f9b7e03b64f814937814
Kapil Koju <960364+TheKojuEffect@users.noreply.github.com>
2022-10-13 20:09:02 -0400

    Add Location table and data

--- repo/src/main/resources/db/migration/V2__add_state_table.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE state (
-    abbreviation CHAR(2) PRIMARY KEY,
-    name         VARCHAR(30) NOT NULL
-);
--- repo/src/main/resources/db/migration/V4__add_city_table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE city (
-    id    UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name  VARCHAR(50) NOT NULL,
-    state CHAR(2) REFERENCES state(abbreviation)
-);
