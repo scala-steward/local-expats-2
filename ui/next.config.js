806afc278283df82546d92716027d20d665f543f
Kapil Koju <960364+TheKojuEffect@users.noreply.github.com>
2022-10-19 00:17:17 -0400

    Disable PWA in development

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 
 const withPWA = require('next-pwa')({
-    dest: 'public'
+    dest: 'public',
+    // To fix an issue with webpack --watch mode in development
+    // https://github.com/GoogleChrome/workbox/issues/1790
+    disable: process.env.NODE_ENV === 'development'
 });
 
 module.exports = withPWA({
