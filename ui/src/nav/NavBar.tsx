176c6404be19abbb46676dbe5c1da0fa3f2be2fc
Kapil Koju <960364+TheKojuEffect@users.noreply.github.com>
2022-10-21 01:09:34 -0400

    Add AppIcon

--- ui/src/nav/NavBar.tsx
+++ ui/src/nav/NavBar.tsx
@@ -1,43 +1,63 @@
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Link from "next/link";
 import {LocationSelect} from "../location/LocationSelect";
 import {useSelectedLocation} from "../location/SelectedLocation";
 import {AddNew} from "./AddNew";
 import {Notification} from "./Notification";
+import IconButton from "@mui/material/IconButton";
+import {AppIcon} from "./AppIcon";
+import Link from "next/link";
+import {useSmallScreen} from "../util/useUtils";
+import Typography from "@mui/material/Typography";
+import {red} from "@mui/material/colors";
 
 export default function NavBar() {
     const {selectedLocation, setSelectedLocation, setSelectedLocationToDefault} = useSelectedLocation();
+
+    const smallScreen = useSmallScreen();
     return (
         <Box>
             <AppBar position="fixed">
                 <Toolbar>
                     <Link href="/">
-                        <Typography
-                            variant="h6"
-                            noWrap
-                            sx={{
-                                cursor: "pointer",
-                                caretColor: "transparent"
-                            }}
-                            onClick={setSelectedLocationToDefault}
-                        >
-                            NepaliUS
-                        </Typography>
+                        <Box onClick={setSelectedLocationToDefault} sx={{display: "flex", alignItems: "center"}}>
+                            <IconButton
+                                size="large"
+                            >
+                                <AppIcon fontSize="large"/>
+                            </IconButton>
+
+                            {
+                                !smallScreen && (
+                                    <Typography
+                                        variant="h6"
+                                        noWrap
+                                        sx={{
+                                            cursor: "pointer",
+                                            caretColor: "transparent"
+                                        }}
+
+                                    >
+                                        NepaliUS
+                                    </Typography>
+                                )
+                            }
+                        </Box>
                     </Link>
+
+
                     <Box sx={{
                         flexGrow: 1,
-                        mx: {xs: 1, sm: 2},
-                        width: 140
+                        ml: {xs: 1, sm: 3},
+                        maxWidth: '350px'
                     }}>
                         <LocationSelect
                             value={selectedLocation}
                             onChange={setSelectedLocation}
                         />
                     </Box>
-                    <Box sx={{flexGrow: 1}}/>
+                    {!smallScreen && <Box sx={{flexGrow: 1}}/>}
                     <Box sx={{display: {sm: 'flex'}, ml: 2}}>
                         <Notification/>
                         <AddNew/>
