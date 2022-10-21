d17ef33f214b9094f98cc6b8cfed6f0810c3a4e2
Kapil Koju <960364+TheKojuEffect@users.noreply.github.com>
2022-10-20 22:28:59 -0400

    UI for notifications

--- ui/src/nav/Notification.tsx
+++ ui/src/nav/Notification.tsx
@@ -1,11 +1,14 @@
 import IconButton from "@mui/material/IconButton";
-import {Badge} from "@mui/material";
-import {Notifications} from "@mui/icons-material";
-import {FC} from "react";
+import {Badge, ListItemIcon, ListItemText, Menu, MenuItem} from "@mui/material";
+import {Notifications, QuestionAnswer} from "@mui/icons-material";
+import React, {FC, useState} from "react";
 import {get} from "../util/Fetch";
 import {PostDto} from "../post/PostDto";
 import {useQuery} from "@tanstack/react-query";
 import {usePostBookmarks} from "../post/PostBookmarks";
+import Tooltip from "@mui/material/Tooltip";
+import {PostLink} from "../post/PostLink";
+import {useSmallScreen} from "../util/useUtils";
 
 export const Notification: FC = () => {
     const {postIds} = usePostBookmarks();
@@ -14,21 +17,69 @@ export const Notification: FC = () => {
         ids,
         since: new Date().toISOString(),
     });
-    const {isLoading, data: updates} = useQuery(['notifications', ids], fetchUpdatedPosts, {
+    const {data} = useQuery(['notifications', ids], fetchUpdatedPosts, {
         refetchOnWindowFocus: false,
         refetchOnMount: false,
         refetchOnReconnect: false,
         refetchInterval: 3 * 60 * 1000, // 3 minutes
     });
+    const updatedPosts = data ?? [];
+
+    const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
+    const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElUser(event.currentTarget);
+    };
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    const smallScreen = useSmallScreen();
 
     return (
-        <IconButton
-            size="large"
-            color="inherit"
-        >
-            <Badge badgeContent={updates?.length} color="info">
-                <Notifications/>
-            </Badge>
-        </IconButton>
+        <>
+            <Tooltip title="Show notifications">
+                <IconButton onClick={handleOpenUserMenu}
+                            size="large"
+                            color="inherit"
+                >
+                    <Badge badgeContent={updatedPosts.length} color="info">
+                        <Notifications/>
+                    </Badge>
+                </IconButton>
+            </Tooltip>
+            <Menu
+                sx={{mt: 5}}
+                anchorEl={anchorElUser}
+                keepMounted
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+                PaperProps={{
+                    style: {
+                        width: smallScreen ? '100%' : '350px'
+                    }
+                }}
+            >
+                {updatedPosts.map((post) => (
+                    <PostLink post={post} key={post.id}>
+                        <MenuItem onClick={handleCloseUserMenu}>
+                            <ListItemIcon>
+                                <Badge badgeContent={2} color="info">
+                                    <QuestionAnswer fontSize="small"/>
+                                </Badge>
+                            </ListItemIcon>
+                            <ListItemText secondary={post.title} secondaryTypographyProps={{noWrap: true}}/>
+                        </MenuItem>
+                    </PostLink>
+                ))}
+            </Menu>
+        </>
     );
 }
