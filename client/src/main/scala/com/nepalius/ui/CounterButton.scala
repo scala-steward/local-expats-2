939734fb288e2c43d74a44a88f0105aa390361d5
Kapil Koju <960364+TheKojuEffect@users.noreply.github.com>
2023-11-09 09:51:43 -0500

    Setup tailwind

--- client/src/main/scala/com/nepalius/ui/CounterButton.scala
+++ client/src/main/scala/com/nepalius/ui/CounterButton.scala
@@ -6,6 +6,7 @@ def CounterButton(): Element =
   val counter = Var(0)
   button(
     tpe := "button",
+    cls := "bg-blue-500",
     "Count is ",
     child.text <-- counter,
     onClick --> { _ => counter.update(_ + 1) },
