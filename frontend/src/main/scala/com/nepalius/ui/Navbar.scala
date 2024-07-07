15c2a4b232f92be1c339503bf940c97cb3040d46
Kapil Koju <960364+TheKojuEffect@users.noreply.github.com>
2024-07-07 15:40:11 -0400

    Remove unused anchors

--- frontend/src/main/scala/com/nepalius/ui/Navbar.scala
+++ frontend/src/main/scala/com/nepalius/ui/Navbar.scala
@@ -4,18 +4,8 @@ import com.raquo.laminar.api.L.*
 import tailwind.*
 
 def Navbar(): HtmlElement =
-  div(
-    tw.h_8.bg_blue_500,
-    div(
-      tw.max_w_lg.mx_auto
-        .h_full
-        .flex.justify_between.items_center,
-      a(tw.float_left, href := "/", "NepaliUS"),
-      LocationSelect(),
-      div(
-        tw.float_right.flex.justify_between,
-        a(tw.px_2, href := "/new", "New"),
-        a(tw.px_2, href := "/login", "Login"),
-      ),
-    ),
+  navTag(
+    tw.h_full,
+    a(tw.mx_4, href := "/", "NepaliUS"),
+    LocationSelect(),
   )
